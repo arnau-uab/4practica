546dbd9622ce1fd472d3cdc5c787347dbdb2e9e9
dani <1745420@uab.cat>
2024-12-14 13:02:55 +0100

    versio5

--- my_tee.c
+++ my_tee.c
@@ -3,6 +3,23 @@
 #include <unistd.h>
 
 int main (int argc, char *argv[]){
+	bool actiu = True;
 
 	char cadena = argv[1];
-	
+	fd = open(./cadena, O_WRONLY);
+
+	if (fd == -1) {
+    		perror("Error: no s'ha pogut crear un arxiu");
+		exit(-1);
+	}
+
+	char *array[]
+
+	while(actiu){
+		char linia = scanf();
+		for(int i = 0; i>=0; i++){
+			array[i] = linia
+		}
+		for(int j = 0; i>=0; i++){
+			write(fd, void j, );
+
